6dd5c0504dbd7bfb9a47105823a5448c91885ee6
jingleheimer-schmidt <46385945+jingleheimer-schmidt@users.noreply.github.com>
2023-05-16 01:11:47 +0200

    Create settings.lua

---
+++ settings.lua
@@ -0,0 +1,21 @@
+
+local brightness_multiplier = {
+    type = "string-setting",
+    name = "brightness_multiplier",
+    setting_type = "startup",
+    default_value = "0.5",
+    allowed_values = {
+        "1",
+        "0.75",
+        "0.66",
+        "0.5",
+        "0.33",
+        "0.25",
+        "0.11"
+    },
+    order = "a-1"
+}
+
+data:extend({
+    brightness_multiplier
+})
