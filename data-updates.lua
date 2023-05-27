62df9a541d2b824b59c8d283a1f915ab81149161
jingleheimer-schmidt <46385945+jingleheimer-schmidt@users.noreply.github.com>
2023-05-27 00:21:50 -0700

    resolved error when loading with mods that add lamps with no light definition

--- data-updates.lua
+++ data-updates.lua
@@ -4,6 +4,10 @@ require ("util")
 local intensity_multiplier = tonumber(settings.startup["brightness_multiplier"].value)
 
 for _, lamp in pairs(data.raw.lamp) do
-    lamp.light.intensity = lamp.light.intensity * intensity_multiplier
-    lamp.glow_color_intensity = lamp.glow_color_intensity * intensity_multiplier
+    if lamp.light then
+        lamp.light.intensity = lamp.light.intensity * intensity_multiplier
+    end
+    if lamp.glow_color_intensity then
+        lamp.glow_color_intensity = lamp.glow_color_intensity * intensity_multiplier
+    end
 end
