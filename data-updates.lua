2e8757b9e76d13bb463fe773dfe6f9fcc6fcd9f6
asher_sky <46385945+jingleheimer-schmidt@users.noreply.github.com>
2024-11-21 19:32:35 -0800

    handle lamp.light being a struct or struct[]

--- data-updates.lua
+++ data-updates.lua
@@ -13,8 +13,16 @@ local function adjust_light_intensity(light)
 end
 
 for _, lamp in pairs(data.raw.lamp) do
-    if lamp.light then
-        lamp.light.intensity = lamp.light.intensity * intensity_multiplier
+    local light = lamp.light
+    if light then
+        if light[1] then
+            for _, light_instance in pairs(light) do
+                adjust_light_intensity(light_instance)
+            end
+        else
+            adjust_light_intensity(light)
+        end
+    end
     end
     if lamp.glow_color_intensity then
         lamp.glow_color_intensity = lamp.glow_color_intensity * intensity_multiplier
