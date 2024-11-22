55fd1e0b3cd9c0a0086b50717f858b575305c343
asher_sky <46385945+jingleheimer-schmidt@users.noreply.github.com>
2024-11-21 19:47:44 -0800

    deduplicate code

--- data-updates.lua
+++ data-updates.lua
@@ -6,33 +6,34 @@ local intensity_multiplier = tonumber(settings.startup["brightness_multiplier"].
 ---@param light data.LightDefinition.struct
 local function adjust_light_intensity(light)
     light.intensity = light.intensity * intensity_multiplier
-    -- light.flicker_interval = 15
-    -- light.flicker_min_modifier = .9
-    -- light.flicker_max_modifier = 1
+end
+
+---@param light data.LightDefinition.struct
+local function adjust_light_flicker(light)
+    light.flicker_interval = 50
+    light.flicker_min_modifier = .9
+    light.flicker_max_modifier = 1
     light.offset_flicker = true
 end
 
-for _, lamp in pairs(data.raw.lamp) do
-    local light = lamp.light
+---@param light data.LightDefinition
+local function process_lights(light)
     if light then
         if light[1] then
             for _, light_instance in pairs(light) do
                 adjust_light_intensity(light_instance)
+                adjust_light_flicker(light_instance)
             end
         else
             adjust_light_intensity(light)
+            adjust_light_flicker(light)
         end
     end
-    local light_when_colored = lamp.light_when_colored
-    if light_when_colored then
-        if light_when_colored[1] then
-            for _, light_instance in pairs(light_when_colored) do
-                adjust_light_intensity(light_instance)
-            end
-        else
-            adjust_light_intensity(light_when_colored)
-        end
-    end
+end
+
+for _, lamp in pairs(data.raw.lamp) do
+    process_lights(lamp.light)
+    process_lights(lamp.light_when_colored)
     if lamp.glow_color_intensity then
         lamp.glow_color_intensity = lamp.glow_color_intensity * intensity_multiplier
     end
