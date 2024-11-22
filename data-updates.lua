dc92fe3a55cdc1ed26a21918a7690ec7a9b968d9
asher_sky <46385945+jingleheimer-schmidt@users.noreply.github.com>
2024-11-21 19:32:01 -0800

    breakout light modification

--- data-updates.lua
+++ data-updates.lua
@@ -3,6 +3,15 @@ require("util")
 
 local intensity_multiplier = tonumber(settings.startup["brightness_multiplier"].value)
 
+---@param light data.LightDefinition.struct
+local function adjust_light_intensity(light)
+    light.intensity = light.intensity * intensity_multiplier
+    -- light.flicker_interval = 15
+    -- light.flicker_min_modifier = .9
+    -- light.flicker_max_modifier = 1
+    light.offset_flicker = true
+end
+
 for _, lamp in pairs(data.raw.lamp) do
     if lamp.light then
         lamp.light.intensity = lamp.light.intensity * intensity_multiplier
